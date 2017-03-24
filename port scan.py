f11dd8f7d1df26b3c5671c7c1d5c7f05d79d80f3
VirusBaghdad <aselmohmmed65@gmail.com>
2017-03-24 16:16:03 +0300

    Add files via upload

---
+++ port scan.py
@@ -0,0 +1,24 @@
+from socket import *
+print '''
+                   _                         
+                  | |                        
+  _ __   ___  _ __| |_   ___  ___ __ _ _ __  
+ | '_ \ / _ \| '__| __| / __|/ __/ _` | '_ \ 
+ | |_) | (_) | |  | |_  \__ \ (_| (_| | | | |
+ | .__/ \___/|_|   \__| |___/\___\__,_|_| |_|
+ | |                                         
+ |_|                                        
+by virus baghdad
+
+'''
+target = raw_input ("Enter host to scan :")
+TRIP = gethostbyname(target)
+print "scan to host ip=", TRIP
+
+for i in range (20, 1000):
+    f = socket(AF_INET, SOCK_STREAM)
+
+    x = f.connect_ex((TRIP, i))
+
+    if(x == 0) :
+        print "print open =", i
